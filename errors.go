5ba3d0bce406b4b4ce694ca4387b9d086ac0c10f
Ethan Buchman <ethan@coinculture.info>
2017-06-20 20:53:41 -0400

    errNoData

--- errors.go
+++ errors.go
@@ -6,6 +6,8 @@ import (
 	"github.com/pkg/errors"
 )
 
+//--------------------------------------------
+
 type errHeightMismatch struct {
 	h1, h2 int
 }
@@ -27,3 +29,26 @@ func ErrHeightMismatch(h1, h2 int) error {
 	err := errHeightMismatch{h1, h2}
 	return errors.WithStack(err)
 }
+
+//--------------------------------------------
+
+type errNoData struct{}
+
+func (e errNoData) Error() string {
+	return fmt.Sprintf("No data returned for query")
+}
+
+// IsNoDataErr checks whether an error is due to a query returning empty data
+func IsNoDataErr(err error) bool {
+	if err == nil {
+		return false
+	}
+	_, ok := errors.Cause(err).(errNoData)
+	return ok
+}
+
+func ErrNoData() error {
+	return errors.WithStack(errNoData{})
+}
+
+//--------------------------------------------
